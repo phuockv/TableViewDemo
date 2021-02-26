218ba3b30ecb99f9b262fd4ece08d37ef522e62f
Phuoc <phuoc@buuuk.com>
2021-02-26 22:24:09 +0700

    add header

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
     
     
     var countries = [
-        Country(isoCode: "at", name: "Austria"),
-        Country(isoCode: "be", name: "Belgium"),
-        Country(isoCode: "de", name: "Germany"),
-        Country(isoCode: "el", name: "Greece"),
-        Country(isoCode: "fr", name: "France"),
+        [
+            Country(isoCode: "at", name: "Austria"),
+            Country(isoCode: "be", name: "Belgium"),
+            Country(isoCode: "de", name: "Germany"),
+            Country(isoCode: "el", name: "Greece"),
+            Country(isoCode: "fr", name: "France"),
+        ],
+        [
+            Country(isoCode: "at", name: "Austria"),
+            Country(isoCode: "be", name: "Belgium"),
+            Country(isoCode: "de", name: "Germany")
+        ]
     ]
     
 
@@ -27,6 +34,9 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isEditing = true
+        
+        self.tableView.sectionHeaderHeight = UITableView.automaticDimension
+        self.tableView.estimatedSectionHeaderHeight = 250
     }
 
 
@@ -36,21 +46,47 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return countries.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countries.count
+        return countries[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContriesTableViewCell", for: indexPath) as! ContriesTableViewCell
-        cell.contentImage.image = UIImage(named: countries[indexPath.row].isoCode)
-        cell.contentLabel.text = countries[indexPath.row].name
-
+        cell.contentImage.image = UIImage(named: countries[indexPath.section][indexPath.row].isoCode)
+        cell.contentLabel.text = countries[indexPath.section][indexPath.row].name
+        
         return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: tableView.frame.size.height))
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        headerView.addSubview(label)
+        label.numberOfLines = 0
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -20),
+            label.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 10),
+            label.bottomAnchor.constraint(equalTo: headerView.bottomAnchor, constant: -10)
+        ])
+
+        if section == 0 {
+            headerView.backgroundColor = .red
+            label.text = "Header section 1 eader section 1 eader section 1 eader section 1 eader section 1 eader section 1 eader section 1 eader section 1"
+        } else {
+            headerView.backgroundColor = .blue
+            label.text = "Header section 2"
+        }
+        return headerView
+
+    }
+  
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(countries[indexPath.row])
     }
